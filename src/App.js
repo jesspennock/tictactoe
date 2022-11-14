520f2c10fda4876abe126ba9074b44aaaa484d76
Jess Pennock <jess.pennock@gmail.com>
2022-11-13 22:34:46 -0700

    tic tac toe weekly project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Square from './Square';
 
 function App() {
+  const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
+  const [player, setPlayer] = useState(true);
+  
+  const handleClick = () => {
+    setSquares(["","","","","","","","",""])
+    setPlayer(true)
+  }
+
+  const calculateWinner = (arr) => {
+    let lines = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [0, 4, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]
+    for(let i=0; i<lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (
+        arr[a] &&
+        arr[a] === arr[b] &&
+        arr[a] === arr[c]
+      ) {
+        return `${arr[a]} won!`
+      }
+    }
+    return "Who will win?"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {squares.map((val, index)=> {
+          return(
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={val}
+              index={index}
+            />
+          )
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
+      <div>
+        <span>{calculateWinner(squares)}</span>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
